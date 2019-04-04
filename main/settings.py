c800e4f4319ed1092f9ae75f865e01538fd00359
Tom Riat <riat.tom@gmail.com>
2019-04-04 07:26:19 +0200

    feat: setup postgresql

--- main/settings.py
+++ main/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yogi_web',
+        'USER': 'yogi',
+        'PASSWORD': 'yogi',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
