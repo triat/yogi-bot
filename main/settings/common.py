6dda3461399ebea4f3a5f5ceea3e6e85ebf399bb
Tom Riat <riat.tom@gmail.com>
2019-09-25 20:53:38 +0200

    feat: new config for DB

--- main/settings/common.py
+++ main/settings/common.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,17 +76,10 @@ WSGI_APPLICATION = "main.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "yogi_web",
-        "USER": "yogi",
-        "PASSWORD": "yogi",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
-}
-
+DATABASES = {}
+DATABASES["default"] = dj_database_url.config(
+    conn_max_age=600, default="postgres://yogi:yogi@localhost:5432/yogi_web"
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
